c88cf1f00a1fc16a1663308127891a2fe1c722db
Mihai Statie <57944914+mihaistatie@users.noreply.github.com>
2020-05-14 16:54:27 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 //Creating variable for button, body & the span
-const hexBtn= document.querySelector('.hexBtn');
-const bodyBcg= document.querySelector('body');
+const hexBtn = document.querySelector('.hexBtn');
+const bodyBcg = document.querySelector('body');
 const hex = document.querySelector('.hex')
 
 //Creating and array of hex colors that will use
-const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 //Creating a click event for functioin get HEx
-hexBtn.addEventListener('click',getHex);
+hexBtn.addEventListener('click', getHex);
 
-function getHex () {
-  let hexCol ='#';
+function getHex() {
+  let hexCol = '#';
 
-  for(let i=0; i<6; i++){
-    let random = Math.floor(Math.random()*hexNumbers.length);
-    hexCol= hexCol + hexNumbers[random];
+  for (let i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * hexNumbers.length);
+    hexCol = hexCol + hexNumbers[random];
 
   }
   bodyBcg.style.backgroundColor = hexCol;
-  hex.innerHTML= hexCol;
+  hex.innerHTML = hexCol;
 }
